cda6a06f96299bddc29a2f023bc0bee6d3ea3fdc
Louis Tao <louistao630@sina.com>
2016-02-11 05:06:49 +0800

    exercise save simply object

--- src/main/java/com/louis/exercise/hibernate/App.java
+++ src/main/java/com/louis/exercise/hibernate/App.java
@@ -1,25 +1,37 @@
 package com.louis.exercise.hibernate;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.louis.exercise.hibernate.data.Message;
+import com.louis.exercise.hibernate.util.HibernateUtil;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-       Session session= HibernateUtil.getSessionFactory().openSession();
-       Transaction tx = session.beginTransaction();
-       Message msg = new Message("Hello World!");
-       session.save(msg);
-       tx.commit();
-       session.close();
-       
-       HibernateUtil.shutdown();
-    }
+public class App {
+	public static void main(String[] args) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		Message msg = new Message("Hello World!");
+		session.save(msg);
+		tx.commit();
+		session.close();
+
+		session = HibernateUtil.getSessionFactory().openSession();
+		tx = session.beginTransaction();
+		List<?> messages = session.createQuery("from Message m order by m.text asc").list();
+		System.out.println(messages.size() + " message(s) found:");
+		for (java.util.Iterator<?> it = messages.iterator(); it.hasNext();) {
+			Message loadMessage = (Message) it.next();
+			System.out.println(loadMessage.getText());
+		}
+		tx.commit();
+		session.close();
+
+		HibernateUtil.shutdown();
+	}
 }
--- src/main/java/com/louis/exercise/hibernate/util/HibernateUtil.java
+++ src/main/java/com/louis/exercise/hibernate/util/HibernateUtil.java
@@ -1,4 +1,4 @@
-package com.louis.exercise.hibernate;
+package com.louis.exercise.hibernate.util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
